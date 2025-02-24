d99b9986e1c93444ad1ac45208a32d77d4a7e32e
Sagar <sagarsridhar266@gmail.com>
2025-02-24 23:30:59 +0530

    Updated live logging

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,5 +1,9 @@
 import requests
+import logging
 
+# initialize the logger
+logging.basicConfig(level=logging.INFO)
+logger=logging.getLogger(__name__)
 class APIClient:
     def __init__(self):
         self.base_url="https://reqres.in/api"
@@ -7,5 +11,14 @@ class APIClient:
     def create_user(self,name,job):
         url=f"{self.base_url}/users"
         payload={"name":name,"job":job}
+        # log the API request
+        logger.info(f"Sending POST request(url)")
+        logger.info(f"Payload:{payload}")
         response=requests.post(url,json=payload)
-        return response.json()
+        response_data=response.json()
+
+        # log the API response
+        logger.info(f"Response Status Code:{response.status_code}")
+        logger.info(f"API Response:{response_data}")
+        
+        return response_data
